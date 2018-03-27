049847681886a8af4c8006e166743209bb792339
akhyatf <akhyatf706@gmail.com>
2018-03-27 09:39:05 +0800

    mengisi nilai attribut

--- src/praktek02/kainaksi.java
+++ src/praktek02/kainaksi.java
@@ -5,6 +5,8 @@ public class kainaksi {
     public static void main(String[] args) {
         
          kain A1 = new kain();
+         A1.panjang = 10;
+         A1.lebar = 20;
         
     }
     
