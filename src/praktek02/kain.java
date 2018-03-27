bd1c4a2e4ebd7948e05abb3b480792d2c4bfe0b7
akhyatf <akhyatf706@gmail.com>
2018-03-27 09:31:49 +0800

    membuat attribut dan behavior

--- src/praktek02/kain.java
+++ src/praktek02/kain.java
@@ -2,5 +2,14 @@ package praktek02;
 
 
 public class kain {
+    double panjang;
+    double lebar;
+    
+    void cetakInfo(){
+        System.out.println("Panjang         :"+panjang);
+        System.out.println("Lebar           :"+lebar);
+
+
+}
     
 }
